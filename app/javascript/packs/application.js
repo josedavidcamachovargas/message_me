53cdc19468490d830f3ac44d2edbae78e5baa0b8
Jose David <jose.camacho@ucrso.info>
2022-01-06 23:23:47 -0600

    Enable flash messages display

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,4 +28,7 @@ console.log('Hello World from Webpacker')
 
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
 })
